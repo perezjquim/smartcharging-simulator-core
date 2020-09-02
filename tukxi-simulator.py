606efa9634e5413b6fcef7eb2aa801d755ceb045
Joaquim Perez <jquim1@hotmail.com>
2020-09-02 20:22:37 +0100

    init

---
+++ tukxi-simulator.py
@@ -0,0 +1,11 @@
+from algorithms import *
+from core import *
+from data import *
+from models import *
+from ui import *
+
+def main( ):
+	print( "Great success" )
+
+if __name__ == '__main__':
+	main( )
