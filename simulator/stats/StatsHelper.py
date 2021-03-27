276a6d217dc95aaae2d58bef5602e82a52b8fd7c
Joaquim Perez <jquim1@hotmail.com>
2021-03-27 16:08:49 +0000

    googlecharts -> chartjs

--- simulator/stats/StatsHelper.py
+++ simulator/stats/StatsHelper.py
@@ -1,3 +1,4 @@
+import random
 from base.SingletonMetaClass import SingletonMetaClass
 from core.CarStatuses import CarStatuses
 
@@ -16,44 +17,38 @@ class StatsHelper( metaclass = SingletonMetaClass ):
 		cars = sim_data[ 'cars' ]
 		plugs = sim_data[ 'plugs' ]		
 
-		car_stats_cols = self._car_stats[ 'columns' ]			
-		if len( car_stats_cols ) == 1:
-			for c in cars:
-				car_stats_cols.append( { 'type': 'number', 'label': 'Car #{}'.format( c[ 'id' ] ) } )
+		sim_datetime_str = sim_data[ 'sim_datetime' ]		
 
-		plug_stats_cols = self._plug_stats[ 'columns' ]
-		if len( plug_stats_cols ) == 1:
-			for p in plugs:
-				plug_stats_cols.append( { 'type': 'number', 'label': 'Plug #{}'.format( p[ 'id' ] ) } )				
+		self._prepare_datasets( cars, plugs )
 
-		# update, if necessary
-		sim_datetime_str = sim_data[ 'sim_datetime' ]
+		# update data, if necessary
 		if ( not self._last_datetime_str ) or ( self._last_datetime_str != sim_datetime_str ):
+								
+			self._update_labels( sim_datetime_str )
 			
-			car_stats_new_row = [ sim_datetime_str ]
-			for c in cars:
-				car_stats_new_row.append( c[ 'battery_level' ] )
+			for i in range( len( cars ) ):
+				car = cars[ i ]
+				car_battery_level = car[ 'battery_level' ]
+
+				car_stats_datasets = self._car_stats[ 'datasets' ]
+				car_stats_data = car_stats_datasets[ i ][ 'data' ]
+				car_stats_data.append( car_battery_level )
 			
-			self._car_stats[ 'rows' ].append( car_stats_new_row )
+			for i in range( len( plugs ) ):
+				plug = plugs[ i ]
+				plug_energy_consumption = plug[ 'energy_consumption' ]
 
-			plug_stats_new_row = [ sim_datetime_str ]
-			for p in plugs:
-				plug_stats_new_row.append( p[ 'energy_consumption' ] )
+				plug_stats_datasets = self._plug_stats[ 'datasets' ]
+				plug_stats_data = plug_stats_datasets[ i ][ 'data' ]
+				plug_stats_data.append( plug_energy_consumption )	
 
-			self._plug_stats[ 'rows' ].append( plug_stats_new_row )
-
-			travel_stats_new_row = [ sim_datetime_str ]
 			number_of_traveling_cars = len( [ c for c in cars if c[ 'status' ] ==  CarStatuses.STATUS_TRAVELING ] )
-			travel_stats_new_row.append( number_of_traveling_cars )
-
-			self._travel_stats[ 'rows' ].append( travel_stats_new_row )
+			travel_stats_datasets = self._travel_stats[ 'datasets' ]
+			travel_stats_data = travel_stats_datasets[ 0 ][ 'data' ]
+			travel_stats_data.append( number_of_traveling_cars )	
 
 	def get_stats( self ):
 		return {
-			'home_stats' : {
-				'columns' : [ { 'type': 'string', 'label': 'Task' }, { 'type': 'number', 'label': 'Hours per Day' } ],
-				'rows' : [ [ 'Work', 11 ], [ 'Eat', 2 ], [ 'Watch TV', 4 ] ]
-			},
 			'car_stats' : self._car_stats,
 			'plug_stats' : self._plug_stats,
 			'travel_stats' : self._travel_stats
@@ -61,14 +56,71 @@ class StatsHelper( metaclass = SingletonMetaClass ):
 
 	def clean_up( self ):
 		self._car_stats = {
-	                'columns' : [ { 'type': 'date', 'label': 'Date' } ],
-	                'rows' : [ ]
+	                'labels' : [ ],
+	                'datasets' : [ ]
             	}
 		self._travel_stats = {
-			'columns' : [ { 'type': 'date', 'label': 'Date' }, { 'type': 'number', 'label': 'No. of traveling cars' } ],
-	                'rows' : [ ]
+	                'labels' : [ ],
+	                'datasets' : [ ]
 		}
 		self._plug_stats = {
-			'columns' : [ { 'type': 'date', 'label': 'Date' } ],
-	                'rows' : [ ]
+	                'labels' : [ ],
+	                'datasets' : [ ]
 		}
+
+	def _prepare_datasets( self, cars, plugs ):
+		car_stats_datasets = self._car_stats[ 'datasets' ]
+		if len( car_stats_datasets ) == 0:
+
+			for c in cars:
+				label = 'Car #{}'.format( c[ 'id' ] )
+				color = self._generate_color( )
+				car_stats_datasets.append( {
+					'label': label,
+					'backgroundColor': color,
+					'borderColor': color,
+					'fill': False,
+					'data': [ ]
+				} )
+
+		plug_stats_datasets = self._plug_stats[ 'datasets' ]
+		if len( plug_stats_datasets ) == 0:
+
+			for p in plugs:
+				label = 'Plug #{}'.format( p[ 'id' ] )
+				color = self._generate_color( )
+				plug_stats_datasets.append( {
+					'label': label,
+					'backgroundColor': color,
+					'borderColor': color,
+					'fill': False,
+					'data': [ ]
+				} )	
+
+		travel_stats_datasets = self._travel_stats[ 'datasets' ]
+		if len( travel_stats_datasets ) == 0:
+
+			label = 'No. of traveling cars'
+			color = self._generate_color( )
+			travel_stats_datasets.append( {
+				'label': label,
+				'backgroundColor': color,
+				'borderColor': color,
+				'fill': False,
+				'data': [ ]
+			} )					
+
+	def _update_labels( self, sim_datetime_str ):
+		car_stats_labels = self._car_stats[ 'labels' ]			
+		car_stats_labels.append( sim_datetime_str )
+
+		plug_stats_labels = self._plug_stats[ 'labels' ]
+		plug_stats_labels.append( sim_datetime_str )
+
+		travel_stats_labels = self._travel_stats[ 'labels' ]			
+		travel_stats_labels.append( sim_datetime_str )	
+
+	def _generate_color( self ):
+		color_hex = hex( random.randint( 0, 0xFFFFFF ) )[ 2: ]
+		color_str = '#{}'.format( color_hex )
+		return color_str
