68ed8bdfd67eec874c508b9137e37d550ad7daf2
Joaquim Perez <jquim1@hotmail.com>
2021-05-05 23:50:23 +0100

    webhook init (2)

--- simulator/main.py
+++ simulator/main.py
@@ -1,8 +1,12 @@
 from flask import Flask
 
+from data.WebhookHelper import WebhookHelper
 from model.DBHelper import DBHelper
 from core.Simulator import Simulator
 from data.DataServer import DataServer
+
+WebhookHelper.attach( )
+WebhookHelper.send_message( 'EnergySim - Core container turned on!', 'SUCCESS' )
 
 db_helper = DBHelper( )
 db_helper.on_init( )
